88f73488ac44d0a704e6080fb87cf26f95bb3291
Matt Porter <mporter@weebly.com>
2015-10-06 14:40:22 -0700

    Reformatted. Now using MutationObserver to listen to dom event changes.

--- files/js/element.js
+++ files/js/element.js
@@ -3,129 +3,160 @@
  * @type {PlatformElement}
  */
 (function() {
-	var Accordion = PlatformElement.extend({
-		initialize: function() {
-			this.activeIndex = this.settings.get('active_index');
+    var Accordion = PlatformElement.extend({
+        initialize: function() {
+            this.activeIndex = this.settings.get('active_index');
 
-			this.fixStyles();
-			this.setupAccordion();
-			this.setOpen();
-		},
+            this.fixStyles();
+            this.setupAccordion();
+            this.setOpen();
+            this.listenToContentChanges();
+        },
 
-		/**
-		 * Styles are applied by default to editable areas of
-		 * the editor. To make the element looks how you want, some styles
-		 * need to be overwritten.
-		 *
-		 * Classes that are used are:
-		 *      - .editable-text
-		 *      - .paragraph
-		 *      - .ui-wrapper
-		 *      - .wsite-image
-		 *      - .wsite-*
-		 *      - (etc...)
-		 */
-		fixStyles: function() {
-			this.$el.find('.editable-text').each(function(index) {
-				$(this).attr('style', '');
-			});
+        /**
+         * Styles are applied by default to editable areas of
+         * the editor. To make the element looks how you want, some styles
+         * need to be overwritten.
+         *
+         * Classes that are used are:
+         *      - .editable-text
+         *      - .paragraph
+         *      - .ui-wrapper
+         *      - .wsite-image
+         *      - .wsite-*
+         *      - (etc...)
+         */
+        fixStyles: function() {
+            this.$el.find('.editable-text').each(function(index) {
+                $(this).attr('style', '');
+            });
 
-			this.$el.find('.element').each(function(index) {
-				$(this).attr('style', '');
-			});
+            this.$el.find('.element').each(function(index) {
+                $(this).attr('style', '');
+            });
 
-			this.fixBoxStyleBorders();
-		},
+            this.fixBoxStyleBorders();
+        },
 
-		/**
-		 * If an index was active when the page was
-		 * reloaded or refreshed, open it back up
-		 */
-		setOpen: function() {
-			/**
-			 * "NA" is the identifier that no items
-			 * were open at page load
-			 */
-			if(this.activeIndex != "NA") {
-				this.$el.find('[data-item="' + this.activeIndex + '"]').find('.accordion__title').trigger('click');
-			}
-		},
+        /**
+         * Listens to subtree modifications in the content areas
+         * and resizes them as needed
+         */
+        listenToContentChanges: function() {
+            /**
+             * Setup a mutation observer to listen to dom manipulation events
+             */
+            var mutationObserver = new MutationObserver(function(mutations) {
+                mutations.forEach(function(mutation) {
+                    var $closest = $(mutation.target).closest('.accordion__content');
+                    if($closest) {
+                        $closest.css({
+                            'max-height': $closest[0].scrollHeight + 20 + 'px' // 20 to compensate for padding
+                        });
+                    }
+                });
+            });
 
-		/**
-		 * Simplistic jQuery usage to animate and control which
-		 * accordion item is currently open
-		 */
-		setupAccordion: function() {
-			var view = this;
+            /**
+             * Assign the mutationObserver to each content area
+             */
+            this.$el.find('.accordion__content').each(function() {
+                mutationObserver.observe($(this)[0], {
+                    childList: true,
+                    subtree: true
+                });
+            });
+        },
 
-			this.$el.find('.accordion__title').on('touchstart click', function(e) {
-				// remove "hover" state on touch events
-				if (e.type == "touchstart") {
-					view.$el.find('.accordion').removeClass('no-touch');
-				}
+        /**
+         * If an index was active when the page was
+         * reloaded or refreshed, open it back up
+         */
+        setOpen: function() {
+            /**
+             * "NA" is the identifier that no items
+             * were open at page load
+             */
+            if (this.activeIndex != "NA") {
+                this.$el.find('[data-item="' + this.activeIndex + '"]').find('.accordion__title').trigger('click');
+            }
+        },
 
-				e.stopPropagation();
-				e.preventDefault();
+        /**
+         * Simplistic jQuery usage to animate and control which
+         * accordion item is currently open
+         */
+        setupAccordion: function() {
+            var view = this;
 
-				var isActive = $(this).parent().hasClass('active');
-				view.settings.set('active_index', 'NA');
+            this.$el.find('.accordion__title').on('touchstart click', function(e) {
+                // remove "hover" state on touch events
+                if (e.type == "touchstart") {
+                    view.$el.find('.accordion').removeClass('no-touch');
+                }
 
-				// handles closing
-				view.$el.find('.accordion__title').each(function() {
-					var $this = $(this);
-					var $next = $(this).next();
-					var eachIsActive = $(this).parent().hasClass('active');
+                e.stopPropagation();
+                e.preventDefault();
 
-					$next.css({
-						'max-height': 0 + 'px'
-					});
+                var isActive = $(this).parent().hasClass('active');
+                view.settings.set('active_index', 'NA');
 
-					if (eachIsActive) {
-						setTimeout(function() {
-							$this.parent().removeClass('active');
-						}, 250);
-					}
-				});
+                // handles closing
+                view.$el.find('.accordion__title').each(function() {
+                    var $this = $(this);
+                    var $next = $(this).next();
+                    var eachIsActive = $(this).parent().hasClass('active');
 
-				// handles opening
-				if (!isActive) {
-					$(this).parent().addClass('active');
-					var $next = $(this).next();
-					var activeIndex = $(this).parent().attr('data-item');
-					view.settings.set('active_index', activeIndex);
-					$next.css({
-						'max-height': $next[0].scrollHeight + 20 + 'px' // 20 to compensate for padding
-					});
-				}
+                    $next.css({
+                        'max-height': 0 + 'px'
+                    });
 
-				view.settings.save();
-			});
+                    if (eachIsActive) {
+                        setTimeout(function() {
+                            $this.parent().removeClass('active');
+                        }, 250);
+                    }
+                });
 
-			this.$el.find('.accordion__title').on('touchend', function() {
-				view.$el.find('.accordion').addClass('no-touch');
-			});
-		},
+                // handles opening
+                if (!isActive) {
+                    $(this).parent().addClass('active');
+                    var $next = $(this).next();
+                    var activeIndex = $(this).parent().attr('data-item');
+                    view.settings.set('active_index', activeIndex);
+                    $next.css({
+                        'max-height': $next[0].scrollHeight + 20 + 'px' // 20 to compensate for padding
+                    });
+                }
 
-		/**
-		 * When using the 'Box' style, to avoid
-		 * thick borders on the top and bottom of
-		 * elements, we just shift all the elements up
-		 * 'i' pixels. Preferable over doing it through css
-		 * because all the items need all 4 borders on hover.
-		 */
-		fixBoxStyleBorders: function() {
-			var view = this;
+                view.settings.save();
+            });
 
-			// only do it if the style is 'box'
-			if (this.settings.get('style') == 'box') {
-				this.$el.find('.accordion--box .accordion__item').each(function(i) {
-					$(this).css({
-						'top': -i
-					});
-				});
-			}
-		}
-	});
+            this.$el.find('.accordion__title').on('touchend', function() {
+                view.$el.find('.accordion').addClass('no-touch');
+            });
+        },
 
-	return Accordion;
+        /**
+         * When using the 'Box' style, to avoid
+         * thick borders on the top and bottom of
+         * elements, we just shift all the elements up
+         * 'i' pixels. Preferable over doing it through css
+         * because all the items need all 4 borders on hover.
+         */
+        fixBoxStyleBorders: function() {
+            var view = this;
+
+            // only do it if the style is 'box'
+            if (this.settings.get('style') == 'box') {
+                this.$el.find('.accordion--box .accordion__item').each(function(i) {
+                    $(this).css({
+                        'top': -i
+                    });
+                });
+            }
+        }
+    });
+
+    return Accordion;
 })();
